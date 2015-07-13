42bb3df70e67d154709c14a447e00036fe7b67c6
Danilo Castro <daniloster@gmail.com>
2015-07-13 13:44:39 +0100

    - Fonts awesome updated to newest version. - Loading component using fonts-awesome spinner. - Reduced requirejs files to just one. - Using script parameters to configure the type of use.

--- Content/Scripts/components/common/toDo/toDo.js
+++ Content/Scripts/components/common/toDo/toDo.js
@@ -29,66 +29,81 @@
                 return {
                     controller: ['$scope', '$rootScope', 'ToDoService', function ($scope, $rootScope, toDoService) {
                       $scope.auth = function(){
+                        loadingController.startLoading();
                         toDoService.straightAccess({ userName: $scope.token })
                         .success(function(data){
                           $scope.user = data;
                           $scope.tasks = data.tasks;
                           $scope.refreshTotals();
+                          loadingController.stopLoading();
                         }).error(function(data){
                           $scope.user = null;
                           $scope.tasks = null;
                           $scope.refreshTotals();
                           $rootScope.updateErrorMessage(data.message);
+                          loadingController.stopLoading();
                         });
                       };
 
                       $scope.saveTask = function(){
                         $rootScope.clearMessages();
+                        loadingController.startLoading();
                         toDoService.addTask($scope.newOne)
                         .success(function(data){
                           $scope.newOne = {};
                           $rootScope.updateSuccessMessage(data.message);
                           $scope.getTasks();
+                          loadingController.stopLoading();
                         })
                         .error(function(data){
                           $rootScope.updateErrorMessage(data.message);
+                          loadingController.stopLoading();
                         })
                       };
 
                       $scope.removeTask = function(id){
                         $rootScope.clearMessages();
+                        loadingController.startLoading();
                         toDoService.removeTask(id)
                         .success(function(data){
                           $rootScope.updateSuccessMessage(data.message);
                           $scope.getTasks();
+                          loadingController.stopLoading();
                         })
                         .error(function(data){
                           $rootScope.updateErrorMessage(data.message);
+                          loadingController.stopLoading();
                         })
                       };
 
                       $scope.toggleTask = function(id){
                         $rootScope.clearMessages();
+                        loadingController.startLoading();
                         toDoService.toggleTask(id)
                         .success(function(data){
                           $scope.getTasks();
                           $rootScope.updateSuccessMessage(data.message);
+                          loadingController.stopLoading();
                         })
                         .error(function(data){
                           $rootScope.updateErrorMessage(data.message);
+                          loadingController.stopLoading();
                         })
                       };
 
                       $scope.getTasks = function(id){
+                        loadingController.startLoading();
                         toDoService.getTasks()
                         .success(function(data){
                           $scope.tasks = data;
                           $scope.refreshTotals();
+                          loadingController.stopLoading();
                         })
                         .error(function(data){
                           $scope.tasks = null;
                           $scope.refreshTotals();
                           $rootScope.updateErrorMessage(data.message);
+                          loadingController.stopLoading();
                         })
                       };
 
